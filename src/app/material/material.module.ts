7eb6f9b0ad5acae67586d4d90076905fa787d97c
akhil-at-816253445251 <akhil-at-816253445251>
2021-05-15 01:49:08 +0530

    cards added

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,15 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 const MaterialModules = [
   MatCardModule,
   MatTabsModule,
   MatIconModule,
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatGridListModule
 ];
 
 @NgModule({
