b030ab0cb27968bcfaf0a5d36f23c24086047f6a
akhil-at-816253445251 <akhil-at-816253445251>
2021-05-14 21:37:06 +0530

    dark mode toggle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: LayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DarkModeService } from 'angular-dark-mode';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  darkMode$ = this.darkModeService.darkMode$;
+  currentTheme: string;
 
-  constructor() { }
+  constructor(private darkModeService: DarkModeService) {
+  }
 
   ngOnInit(): void {
+    this.getCurrentTheme();
+  }
+
+  onToggle(): void {
+    this.darkModeService.toggle();
+    this.getCurrentTheme();
+  }
+
+  getCurrentTheme() {
+    if (JSON.parse(localStorage.getItem('dark-mode')).darkMode)
+      this.currentTheme = 'Dark Mode'
+    else
+      this.currentTheme = 'Light Mode'
   }
 
 }
